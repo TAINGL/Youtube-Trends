727a8a051e87518731581dff824f2cc5252f940a
Laura Taing <laura.taing@hotmail.com>
2020-10-01 16:30:57 +0200

    [data]Update

--- Scrapping/youtube_api.py
+++ Scrapping/youtube_api.py
@@ -17,7 +17,7 @@ pp = pprint.PrettyPrinter(indent=4)
 # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
 # the OAuth 2.0 information for this application, including its client_id and
 # client_secret.
-CLIENT_SECRETS_FILE = "client_secret_2.json" # upload your own key API	
+CLIENT_SECRETS_FILE = "client_secret.json" # upload your own key API	
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.

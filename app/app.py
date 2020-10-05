6c6233906fe70521431507fe5d035b1f1670377e
oorvasi sooprayen <minie_oorvasi@hotmail.com>
2020-10-05 09:14:51 +0200

    flask app

--- app/app.py
+++ app/app.py
@@ -1,34 +1,53 @@
-import os
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, render_template, request
+import csv
 from pymongo import MongoClient
+import pymongo
+from bson.json_util import dumps
+import re
 
-app = Flask(__name__)
+#app initialization
+app = Flask(__name__, template_folder='templates')
 
-client = MongoClient(
-    os.environ['DB_PORT_27017_TCP_ADDR'],
-    27017)
-db = client.tododb
+# global variables for data persistence across requests
+model_output=""
 
+try:
+ client = MongoClient("mongodb+srv://youtuber:youtuber123@youtubers.ffrhx.mongodb.net/test") #host uri
+ print("Connected to Avengers MongoClient Successfully from Project Script!!!")
+except:
+ print("Connection to MongoClient Failed!!!")
 
-@app.route('/')
-def todo():
+db = client.youtube_trends
 
-    _items = db.tododb.find()
-    items = [item for item in _items]
+# main index page route
+@app.route('/', methods=['GET','POST'])
+@app.route('/channels', methods=['GET','POST'])
+def index():
+    channelList = db.channel_info.find(limit=100)
+    if request.method == "POST":
+        #channel search
+        channelSearch = request.form['channelSearch']
+        if channelSearch != "":
+            regex = ".*".join((channelSearch, ".*"))
+            channelList = db.channel_info.find({'title': re.compile(regex, re.IGNORECASE)})
 
-    return render_template('todo.html', items=items)
+    return render_template('index.html',channelList=channelList)
 
+@app.route('/youtuber', methods=['GET','POST'])
+def youtubers():
+    youtuberList = db.youtuber_list.find(limit=100)
+    if request.method == "POST":
+        #youtuber search
+        youtuberSearch = request.form['youtuberSearch']
+        if youtuberSearch != "":
+            regex = ".*".join((youtuberSearch, ".*"))
+            youtuberList = db.youtuber_list.find({'ChannelInfo': re.compile(regex, re.IGNORECASE)})
+    return render_template('youtuber_list.html',youtuberList=youtuberList)
 
-@app.route('/new', methods=['POST'])
-def new():
+@app.route('/dashboard')
+def page_test():
+    return render_template('dashboard.html')
 
-    item_doc = {
-        'name': request.form['name'],
-        'description': request.form['description']
-    }
-    db.tododb.insert_one(item_doc)
-
-    return redirect(url_for('todo'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)

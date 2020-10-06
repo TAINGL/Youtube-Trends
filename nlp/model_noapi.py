af62de641bf984792770b270b63937c09a029dbf
Laura Taing <laura.taing@hotmail.com>
2020-10-06 15:26:47 +0200

    [nlp] model_noapi

---
+++ nlp/model_noapi.py
@@ -0,0 +1,80 @@
+import os
+import pandas as pd
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from transformers import pipeline
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+import sys
+sys.path.insert(0, '/Users/ltaing/Documents/SIMPLON DATA IA/IA/DL PROJET/YOUTUBE/scrapping/')
+import youtube_noapi
+
+
+
+def video_comments(link):
+    """
+    Created comments list, dataframe with comments and id
+    """
+    comment_lists = youtube_noapi.main(link)
+    df = pd.DataFrame(comment_lists, columns =['comments']) 
+    return df, comment_lists
+
+
+def sentiment_analysis(link):
+    """
+    Labelisation comments & count of goob and bad comments
+    """
+    classifier = pipeline('sentiment-analysis')
+    df, comment_lists = video_comments(link)
+    df["label"] = 0
+    for i in range(df.shape[0]):
+        if classifier(comment_lists[i])[0]["label"] == 'POSITIVE':
+            df.label[i] = 'POSITIVE'
+        else:
+            df.label[i] = 'NEGATIVE'
+    print(df)
+    comments_list = df['label'].tolist()
+    count_good = sum(1 for i in comments_list if i == 'POSITIVE')
+    count_bad = sum(1 for i in comments_list if i  == 'NEGATIVE')
+    print(count_good, count_bad)
+    
+    if count_good > count_bad:
+        print("\U0001f600")
+    else:
+        print("\U0001F61E")
+    return df, count_good, count_bad
+
+
+def show_word_cloud(cloud, title):
+    """
+    Print wordcloud
+    """
+    plt.figure(figsize = (16, 10))
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.title(title)
+    plt.axis("off")
+    plt.show()
+
+def comments_splitting(df):
+    """
+    Splitting between positive and negative comments
+    And printing wordcloud associated with the good or bad comment
+    """
+    #df = df.apply(lambda x : text_preprocessing(x))
+    good_reviews = df[df.label == "POSITIVE"]
+    bad_reviews = df[df.label == "NEGATIVE"]
+    #print(good_reviews)
+    good_reviews_text = " ".join(good_reviews.comments.to_numpy().tolist())
+    bad_reviews_text = " ".join(bad_reviews.comments.to_numpy().tolist())
+    good_reviews_cloud = WordCloud(stopwords=STOPWORDS, background_color="white").generate(good_reviews_text)
+    bad_reviews_cloud = WordCloud(stopwords=STOPWORDS, background_color="white").generate(bad_reviews_text)
+    #print(good_reviews_cloud, bad_reviews_cloud)
+    
+    show_word_cloud(good_reviews_cloud, 'good comments')
+    show_word_cloud(bad_reviews_cloud, 'bad comments')
+
+
+df, count_good, count_bad = sentiment_analysis('https://www.youtube.com/watch?v=Kkd9yWak9xw')
+comments_splitting(df)
